5b7d02530d3bb4424d72570dd19d2dcb546fcafe
Koki <glacier.fairytail@gmail.com>
2026-03-31 23:08:20 +0900

    feat:Postモデルの作成

--- my-app/api/db/schema.rb
+++ my-app/api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_31_090202) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_31_140233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,17 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_31_090202) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +54,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_31_090202) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
 end
