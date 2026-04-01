93d4acf308ca36103fbf0dd5e2260b9b7dc7e150
Koki <glacier.fairytail@gmail.com>
2026-04-01 15:44:23 +0900

    feat:アソシエーションの追加

--- my-app/api/app/models/user.rb
+++ my-app/api/app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :categories, dependent: :destroy
+  has_many :posts, dependent: :destroy
 end
