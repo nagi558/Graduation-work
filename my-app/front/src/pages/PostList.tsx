916c653aef9b2b7dcf2d08ae603911a723d0e539
Koki <glacier.fairytail@gmail.com>
2026-04-02 17:40:22 +0900

    feat:伝えたいこと一覧画面のUI作成

--- my-app/front/src/pages/PostList.tsx
+++ my-app/front/src/pages/PostList.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react'
+import axiosInstance from '@/lib/axios'
 import type { Post } from '@/types'
 
 export const PostList = () => {
+  const [posts, setPosts] = useState<Post[]>([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await axiosInstance.get('/api/v1/posts', {
+        headers: {
+          'access-token': localStorage.getItem('access-token'),
+          'client': localStorage.getItem('client'),
+          'uid': localStorage.getItem('uid')
+        }
+      })
+      setPosts(response.data)
+    }
+    fetchPosts()
+  }, [])
+
   return (
     <div>
-      {/* ここに伝えたいこと一覧コンポーネントを表示します。*/}
+      <h1>伝えたいこと一覧</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <p>{post.title}</p>
+            <p>{post.category.name}</p>
+        </div>
+      ))}
     </div>
   )
 }
