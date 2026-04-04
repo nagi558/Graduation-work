9811d331a723a9c4b6d98235d388c97abb051d2e
Koki <glacier.fairytail@gmail.com>
2026-04-04 10:59:19 +0900

    feat:投稿作成時のルーティング

--- my-app/api/app/controllers/api/v1/posts_controller.rb
+++ my-app/api/app/controllers/api/v1/posts_controller.rb
@@ -4,6 +4,36 @@ class Api::V1::PostsController < ApplicationController
   # 一覧取得
   def index
     posts = current_user.posts.includes(:category).order(created_at: :desc)
-    render json: posts.as_json(include: :category), status: :ok
+    render json: posts.as_json(
+      only: [:id, :title, :body, :created_at, :updated_at],
+      include: {
+        category: {
+          only: [:id, :name]
+        }
+      }
+    ), status: :ok
+  end
+
+  # 投稿作成
+  def create
+    post = current_user.posts.build(post_params)
+    if post.save
+      render json: post.as_json(
+        only: [:id, :title, :body, :created_at, :updated_at],
+        include: {
+          category: {
+            only: [:id, :name]
+          }
+        }
+      ), status: :created
+    else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id)
   end
 end
