24e74109691bf6c971289353553b29c562f4be37
Koki <glacier.fairytail@gmail.com>
2026-04-04 22:24:44 +0900

    feat:handleSubmitの追加

--- my-app/front/src/pages/PostNew.tsx
+++ my-app/front/src/pages/PostNew.tsx
@@ -6,14 +6,24 @@ import type { Category } from '@/types'
 export const PostNew = () => {
   const [title, setTitle] = useState('')
   const [body, setBody] =useState('')
-  const [categoryId, setCategoryId] = useState<number : null>(null)
+  const [categoryId, setCategoryId] = useState<number | null>(null)
   const [categories, setCategories] = useState<Category[]>([])
   const [error, setError] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
 
   const navigate = useNavigate()
 
-  // カテゴリー一覧を取得
+  const validateForm = (): boolean => {
+    // フォームの空チェック
+    if (title === "") {
+      setError('タイトルを入力してください')
+      return false
+    }
+
+    return true
+  }
+
+  // カテゴリ一覧を取得
   useEffect(() => {
     const fetchCategories = async () => {
       const accessToken = localStorage.getItem('access-token')
@@ -31,6 +41,37 @@ export const PostNew = () => {
     }
     fetchCategories()
   }, [])
+
+  // フォーム送信
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setError(null)
+
+    // バリデーション実行
+    if (!validateForm()) {
+        return
+    }
+
+    setLoading(true)
+
+    try {
+    // APIリクエスト
+      await axiosInstance.post('/api/v1/posts', {
+        post: {
+          title,
+          body,
+          category_id: categoryId
+        }
+      })
+
+      navigate('/posts')
+
+    } catch (err: any) {
+      setError('投稿を作成できませんでした')
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <div>
