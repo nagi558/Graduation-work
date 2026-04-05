268ee5d89cbde21d38b2d3583b3b5df176d2caab
Koki <glacier.fairytail@gmail.com>
2026-04-05 18:03:00 +0900

    feat:updateとdestroyのルーティング作成

--- my-app/api/app/controllers/api/v1/posts_controller.rb
+++ my-app/api/app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post, only: [:update, :destroy]
 
   # 一覧取得
   def index
@@ -31,7 +32,29 @@ class Api::V1::PostsController < ApplicationController
     end
   end
 
+  # 投稿更新
+  def update
+    if @post.update(post_params)
+      render json: @post.as_json(
+        only: [:id, :title, :body, :created_at, :updated_at],
+        include: { category: { only: [:id, :name] } }
+      ), status: :ok
+    else
+      render json: { errors: @post.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  # 投稿削除
+  def destroy
+    @post.destroy
+    render json: { message: '投稿を削除しました' }, status: :ok
+  end
+
   private
+
+  def set_post
+    @post = current_user.posts.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :body, :category_id)
--- my-app/api/config/routes.rb
+++ my-app/api/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :posts, only: [:index, :create]
+      resources :posts, only: [:index, :create, :update, :destroy]
       resources :categories, only: [:index, :create, :destroy]
     end
   end
