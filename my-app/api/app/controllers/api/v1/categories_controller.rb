0bcfdaccad300f5828aab41b27893945baaa9f35
Koki <glacier.fairytail@gmail.com>
2026-04-07 00:30:06 +0900

    feat:カテゴリのアクション定義

--- my-app/api/app/controllers/api/v1/categories_controller.rb
+++ my-app/api/app/controllers/api/v1/categories_controller.rb
@@ -1,30 +1,54 @@
 class Api::V1::CategoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_category, only: [:show, :update, :destroy]
 
   # 一覧取得
   def index
     categories = current_user.categories.order(created_at: :desc)
-    render json: categories, status: :ok
+    render json: categories.as_json(
+      only: [:id, :name, :created_at, :updated_at]
+    ), status: :ok
   end
 
-  # 作成
+  # カテゴリ作成
   def create
     category = current_user.categories.build(category_params)
     if category.save
-      render json: category, status: :created
+      render json: category.as_json(
+        only: [:id, :name, :created_at, :updated_at]
+      ), status: :created
     else
       render json:{ errors: category.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-  #削除
+  def show
+    render json: @category.as_json(
+      only: [:id, :name, :created_at, :updated_at]
+    ), status: :ok
+  end
+
+  def update
+    if @category.update(category_params)
+      render json: @category.as_json(
+        only: [:id, :name, :created_at, :updated_at]
+      ), status: :ok
+    else
+      render json: { errors: @category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  #カテゴリ削除
   def destroy
-    category = current_user.categories.find(params[:id])
-    category.destroy
+    @category.destroy
     render json: { message: 'カテゴリを削除しました' }, status: :ok
   end
 
   private
+
+  def set_category
+    @category = current_user.categories.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
--- my-app/api/config/routes.rb
+++ my-app/api/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts, only: [:index, :show, :create, :update, :destroy]
-      resources :categories, only: [:index, :create, :destroy]
+      resources :categories, only: [:index, :show, :create, :update, :destroy]
     end
   end
 
