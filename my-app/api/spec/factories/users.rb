c3523fe9625968d9a67ca83b99d286e92f04c995
Koki <glacier.fairytail@gmail.com>
2026-04-13 18:01:49 +0900

    add:factoryの作成

---
+++ my-app/api/spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.name}
+    sequence(:email) { |n| "test#{n}@example.com" }
+    password { 'password123' }
+    password_confirmation { password }
+  end
+end
