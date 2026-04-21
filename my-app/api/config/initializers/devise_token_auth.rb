94d0490dc88ba410c70cdbfdfe440fbdf06abf3b
Koki <glacier.fairytail@gmail.com>
2026-04-21 22:33:14 +0900

    feat:devise_token_auth.rbにパスワードリセット関連の追記

--- my-app/api/config/initializers/devise_token_auth.rb
+++ my-app/api/config/initializers/devise_token_auth.rb
@@ -5,16 +5,17 @@ DeviseTokenAuth.setup do |config|
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
   # each request.
-  # config.change_headers_on_each_request = true
+  config.change_headers_on_each_request = false
 
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
-  # config.token_lifespan = 2.weeks
+  config.token_lifespan = 2.weeks
 
   # Limiting the token_cost to just 4 in testing will increase the performance of
   # your test suite dramatically. The possible cost value is within range from 4
   # to 31. It is recommended to not use a value more than 10 in other environments.
   config.token_cost = Rails.env.test? ? 4 : 10
+  config.reset_password_within = 6.hours
 
   config.bypass_sign_in = false
 
