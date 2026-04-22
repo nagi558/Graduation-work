6248f499bc832be879986c1f4ed9c8f21ff8daf0
Koki <glacier.fairytail@gmail.com>
2026-04-22 13:02:28 +0900

    config/routes.rb に追記

--- my-app/api/config/routes.rb
+++ my-app/api/config/routes.rb
@@ -12,6 +12,14 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    password: 'auth/passwords'
+  }
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
