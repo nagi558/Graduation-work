8391f7daac9aa5f7bba3e0bd411b364bacbd0aad
Koki <glacier.fairytail@gmail.com>
2026-04-27 13:03:57 +0900

    update:posts_controller.rbの修正

--- my-app/api/app/controllers/api/v1/posts_controller.rb
+++ my-app/api/app/controllers/api/v1/posts_controller.rb
@@ -2,56 +2,37 @@ class Api::V1::PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :update, :destroy]
 
-  # 一覧取得
   def index
-    posts = current_user.posts.includes(:category).order(created_at: :desc)
-    render json: posts.as_json(
-      only: [:id, :title, :body, :created_at, :updated_at],
-      include: {
-        category: {
-          only: [:id, :name]
-        }
-      }
-    ), status: :ok
+    posts = current_user.posts
+      .includes(:category)
+      .order(created_at: :desc)
+      .search_title(params[:title])
+      .search_body(params[:body])
+
+    render json: posts, status: :ok
   end
 
-  # 投稿作成
   def create
     post = current_user.posts.build(post_params)
     if post.save
-      render json: post.as_json(
-        only: [:id, :title, :body, :created_at, :updated_at],
-        include: {
-          category: {
-            only: [:id, :name]
-          }
-        }
-      ), status: :created
+      render json: post, status: :created
     else
       render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def show
-    render json: @post.as_json(
-      only: [:id, :title, :body, :created_at, :updated_at],
-      include: { category: {only: [:id, :name] } }
-    ), status: :ok
+    render json: @post, status: :ok
   end
 
-  # 投稿更新
   def update
     if @post.update(post_params)
-      render json: @post.as_json(
-        only: [:id, :title, :body, :created_at, :updated_at],
-        include: { category: { only: [:id, :name] } }
-      ), status: :ok
+      render json: @post, status: :ok
     else
       render json: { errors: @post.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
-  # 投稿削除
   def destroy
     @post.destroy
     render json: { message: '投稿を削除しました' }, status: :ok
