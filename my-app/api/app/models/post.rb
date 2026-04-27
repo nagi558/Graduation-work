0e77f56bc87d9ecada76f39d5812a10bb00320e8
Koki <glacier.fairytail@gmail.com>
2026-04-27 12:55:43 +0900

    fix:post.rbの修正'

--- my-app/api/app/models/post.rb
+++ my-app/api/app/models/post.rb
@@ -3,5 +3,17 @@ class Post < ApplicationRecord
   belongs_to :category
 
   validates :title, presence: true
-  validates :body, presence: :true
+  validates :body, presence: true
+
+  scope :search_title, ->(title) {
+    return all if title.blank?
+    sanitized = ActiveRecord::Base.sanitize_sql_like(title)
+    where('title LIKE ?', "%#{sanitized}%")
+  }
+
+  scope :search_body, ->(body) {
+    return all if body.blank?
+    sanitized = ActiveRecord::Base.sanitize_sql_like(body)
+    where('body LIKE ?', "%#{sanitized}%")
+  }
 end
