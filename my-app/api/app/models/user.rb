0361a0d5c4bb062fd348be4735b9d48ba82a0769
Koki <glacier.fairytail@gmail.com>
2026-04-28 16:06:01 +0900

    update:Userモデルへの追記

--- my-app/api/app/models/user.rb
+++ my-app/api/app/models/user.rb
@@ -8,6 +8,26 @@ class User < ApplicationRecord
 
   after_create :create_default_categories
 
+  def self.find_or_create_by_google(google_uid:, email:, name:)
+    user = find_by(google_uid: google_uid)
+    return user if user
+
+    user = find_by(email: email)
+    if user
+      user.update!(google_uid: google_uid)
+      return user
+    end
+
+    create!(
+      google_uid: google_uid,
+      email:      email,
+      name:       name,
+      uid:        email,
+      provider:   'email',
+      password:   Devise.friendly_token[0, 20]
+    )
+  end
+
   private
 
   def create_default_categories
