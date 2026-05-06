c771c36a06a35061a31e16ab1578696fda5ec8ad
Koki <glacier.fairytail@gmail.com>
2026-05-06 19:34:55 +0900

    refactor: Metrics系のCop指摘に基づきメソッドを分割する

--- my-app/api/app/models/user.rb
+++ my-app/api/app/models/user.rb
@@ -14,20 +14,7 @@ class User < ApplicationRecord
     user = find_by(google_uid: google_uid)
     return user if user
 
-    user = find_by(email: email)
-    if user
-      user.update!(google_uid: google_uid)
-      return user
-    end
-
-    create!(
-      google_uid: google_uid,
-      email:      email,
-      name:       name,
-      uid:        email,
-      provider:   'email',
-      password:   Devise.friendly_token[0, 20]
-    )
+    attach_google_uid_to_existing(email, google_uid) || create_google_user(google_uid, email, name)
   end
 
   def paired?
@@ -35,18 +22,36 @@ class User < ApplicationRecord
   end
 
   def pending?
-    pair.present? && pair.pair_memberships.count == 1
+    pair.present? && pair.pair_memberships.one?
   end
 
   def partner
     return nil unless paired?
+
     pair.users.where.not(id: id).first
   end
 
-  private
+  private_class_method def self.attach_google_uid_to_existing(email, google_uid)
+    user = find_by(email: email)
+    return nil unless user
+
+    user.update!(google_uid: google_uid)
+    user
+  end
+
+  private_class_method def self.create_google_user(google_uid, email, name)
+    create!(
+      google_uid: google_uid,
+      email: email,
+      name: name,
+      uid: email,
+      provider: 'email',
+      password: Devise.friendly_token[0, 20]
+    )
+  end
 
   def create_default_categories
-    ["お金", "仕事", "生活"].each do |name|
+    %w[お金 仕事 生活].each do |name|
       categories.find_or_create_by!(name: name)
     end
   end
